2fd87c7a1bb0022e846eec59ee84338ce5414835
Jonathanaguilera2021 <jaguilera277@gmail.com>
2021-03-15 18:46:25 -0500

    [add] version 1.1 reto rick y morty

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ function search(){
           let html = `<div class="card col-3 ml-10 m-5 p-5" id="aboutSection" style="width: 18rem;">
             <img src="${data.image}" class="card-img-top w-80 ml-2 mt-5  bg-primary" alt="...">
             <div class="card-body">
-              <p class="card-text fs-4"id="charName"> ${data.name}</p>
-              <p class="card-text"id="charSpecies">>${data.species}</p>
-              <p class="card-text"id="charStatus"> ${data.status}</p>
-              <p class="card-text"id="charOrigin">>${data.originname}</p>
-              <p class="card-text"id="charGender">>${data.gender}</p>
+              <p class="card-text fs-3"id="charName"> ${data.name}</p>
+              <p class="card-text"id="charSpecies">Especie:${data.species}</p>
+              <p class="card-text"id="charStatus"> Estatus:${data.status}</p>
+              <p class="card-text"id="charOrigin">Origen:${data.origin.name}</p>
+              <p class="card-text"id="charGender">Genero:${data.gender}</p>
 
              </div>`
 
